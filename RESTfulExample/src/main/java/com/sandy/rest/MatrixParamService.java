7b70878bc46ade2bf3b7636b0d6ef4de544eb430
sandeepdice <rayapudisandeep@gmail.com>
2014-04-28 21:53:01 -0700

    tested matrix param

--- RESTfulExample/src/main/java/com/sandy/rest/MatrixParamService.java
+++ RESTfulExample/src/main/java/com/sandy/rest/MatrixParamService.java
@@ -1,5 +1,16 @@
 package com.sandy.rest;
 
-public class MatrixParamService {
+import javax.ws.rs.GET;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
+// http://localhost:8080/RESTfulExample/rest/matrixparam;param1=p1;param2=p2
+@Path("matrixparam")
+public class MatrixParamService {
+	
+	@GET
+	public Response echoMatrixParams(@MatrixParam("param1") String param1, @MatrixParam("param2") String param2) {
+		return Response.status(200).entity("Received following matrix params: \n param1: "+ param1 + " \nparam2: " + param2).build();
+	}
 }
