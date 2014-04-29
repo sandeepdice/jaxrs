7e4c72341736cc6d3311cd2ef2640641cfa4cca3
sandeepdice <rayapudisandeep@gmail.com>
2014-04-29 03:05:32 -0700

    Form param service. Worked with RESTClient but not from UI directly

--- RESTfulExample/src/main/java/com/sandy/rest/UserFormParamService.java
+++ RESTfulExample/src/main/java/com/sandy/rest/UserFormParamService.java
@@ -7,10 +7,10 @@ import javax.ws.rs.core.Response;
 
 @Path("/user")
 public class UserFormParamService {
-
 	@POST
 	@Path("/add")
 	public Response addUser(@FormParam("name") String name, @FormParam("age") int age) {
+		System.out.println("addUser triggered: name: " + name + " age: " + age);
 		return Response.status(200).entity("addUser is called with: " + name + " age: " + age).build();
 	}
 }
