c0b9d5102e713b6821b40c759f5d39e9c3742f1c
Sandeep <rayapudisandeep@gmail.com>
2014-05-04 19:16:39 -0700

    add test cases

--- RESTfulExample/src/test/java/com/sandy/rest/PackageTest.java
+++ RESTfulExample/src/test/java/com/sandy/rest/PackageTest.java
@@ -7,7 +7,9 @@ import javax.ws.rs.client.WebTarget;
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.sandy.Main;
@@ -16,11 +18,11 @@ import static org.junit.Assert.assertEquals;
 
 public class PackageTest {
 
-    private HttpServer server;
-    private WebTarget target;
+    private static HttpServer server;
+    private static WebTarget target;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setUp() throws Exception {
         // start the server
         server = Main.startServer();
         // create the client
@@ -35,8 +37,8 @@ public class PackageTest {
         target = c.target(Main.BASE_URI);
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @AfterClass
+    public static void tearDown() throws Exception {
         server.stop();
     }
 
@@ -65,5 +67,27 @@ public class PackageTest {
     public void testMatrixParam() {
         String responseMsg = target.path("matrixparam;param1=p1;param2=p2").request().get(String.class);
         assertEquals("Received following matrix params: \n param1: p1 \nparam2: p2", responseMsg);
+    }
+    
+    // TODO: how to post ??
+    public void testFormService() {
+        String name="foo";
+        int age = 23;
+        String responseMsg = target.path("user/add").queryParam("name", name).queryParam("age", age).request().get(String.class);
+
+//        target.property("name", name);
+//        target.property("age", new Integer(age));
+        
+        assertEquals("addUser triggered: name: " + name + " age: " + age, responseMsg);
+    }
+    
+ // TODO: how to send queryparams ??
+    public void testQueryParams() {
+    	String from="a";
+    	String to="b";
+    	
+        String responseMsg = target.path("usersquery").queryParam("from", "a").queryParam("to", "b").request().get(String.class);
+        
+        assertEquals("Received query params from: " + from + " to: " + to, responseMsg);
     }
 }
